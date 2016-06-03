6b30ce3202662190cfabd53b0cf38f6cadb45566
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2016-06-03 15:21:13 +0900

    Prove DRF-PROMISES (WIP)

--- src/drf/PromiseIndep.v
+++ src/drf/PromiseIndep.v
@@ -13,6 +13,7 @@ Require Import Memory.
 Require Import Commit.
 Require Import Thread.
 Require Import Configuration.
+Require Import Progress.
 
 Set Implicit Arguments.
 
@@ -108,16 +109,16 @@ Qed.
  *)
 Inductive race_rw (c:Configuration.t) (ordr ordw:Ordering.t): Prop :=
 | race_rw_intro
-    tid1 lang1 st1 lc1 e1 th1'
-    tid2 lang2 st2 lc2 e2 th2'
+    tid1 lang1 st1 lc1 e1 st1'
+    tid2 lang2 st2 lc2 e2 st2'
     loc
     (TID: tid1 <> tid2)
-    (THREAD1: IdentMap.find tid1 c.(Configuration.threads) = Some (existT _ lang1 st1, lc1))
-    (THREAD2: IdentMap.find tid2 c.(Configuration.threads) = Some (existT _ lang2 st2, lc2))
-    (STEP1: Thread.step e1 (Thread.mk _ st1 lc1 c.(Configuration.memory)) th1')
-    (STEP2: Thread.step e2 (Thread.mk _ st2 lc2 c.(Configuration.memory)) th2')
-    (E1: ThreadEvent.is_reading e1 = Some (loc, ordr))
-    (E2: ThreadEvent.is_writing e1 = Some (loc, ordw))
+    (TH1: IdentMap.find tid1 c.(Configuration.threads) = Some (existT _ lang1 st1, lc1))
+    (TH2: IdentMap.find tid2 c.(Configuration.threads) = Some (existT _ lang2 st2, lc2))
+    (STATE1: lang1.(Language.step) (Some e1) st1 st1')
+    (STATE2: lang2.(Language.step) (Some e2) st2 st2')
+    (EVENT1: ProgramEvent.is_reading e1 = Some (loc, ordr))
+    (EVENT2: ProgramEvent.is_writing e2 = Some (loc, ordw))
 .
 
 Definition pi_racefree (c1:Configuration.t): Prop :=
@@ -127,9 +128,43 @@ Definition pi_racefree (c1:Configuration.t): Prop :=
     <<ORDR: Ordering.le Ordering.acqrel ordr>> /\
     <<ORDW: Ordering.le Ordering.acqrel ordw>>.
 
+Lemma pi_steps_promise
+      tid
+      c1 lang1 st1 lc1
+      c3 lang3 st3 lc3
+      loc from ts msg
+      (STEPS: rtc (pi_step tid) c1 c3)
+      (TH1: IdentMap.find tid c1.(Configuration.threads) = Some (existT _ lang1 st1, lc1))
+      (TH3: IdentMap.find tid c3.(Configuration.threads) = Some (existT _ lang3 st3, lc3))
+      (GET1: Memory.get loc ts lc1.(Local.promises) = Some (from, msg))
+      (GET3: Memory.get loc ts lc3.(Local.promises) = None):
+  exists c2 lang2 st2 lc2 e2 st2' ord2,
+    <<STEPS: rtc (pi_step tid) c1 c2>> /\
+    <<TH2: IdentMap.find tid c2.(Configuration.threads) = Some (existT _ lang2 st2, lc2)>> /\
+    <<STATE2: lang2.(Language.step) (Some e2) st2 st2'>> /\
+    <<EVENT2: ProgramEvent.is_writing e2 = Some (loc, ord2)>> /\
+    <<ORD2: Ordering.le ord2 Ordering.relaxed>>.
+Proof.
+  revert lang1 st1 lc1 from msg TH1 GET1.
+  induction STEPS; i.
+  { rewrite TH1 in TH3. inv TH3. apply inj_pair2 in H1. congr. }
+  inversion H. inv STEP. destruct (Memory.get loc ts lc2.(Local.promises)) as [[]|] eqn:X.
+  { exploit IHSTEPS; s; eauto.
+    { rewrite IdentMap.Facts.add_eq_o; eauto. }
+    i. des. eexists _, _, _, _, _, _, _. splits; [|eauto|eauto|eauto|eauto].
+    etrans; [|eauto]. econs 2; [|econs 1]. eauto.
+  }
+  (* NOTE: maybe it would be better to change Configuration.consistent: split wf & validation *)
+  eexists _, _, _, _, _, _, _. splits; eauto.
+  - admit. (* thread is executed *)
+  - admit. (* the event is writing *)
+  - admit. (* the ordering is <= relaxed *)
+Admitted.
+
 Lemma pi_consistent_small_step_pi
       e tid c1 c2 c3
       (PI_CONSISTENT: pi_consistent c1)
+      (CONSISTENT: Configuration.consistent c1)
       (PI_RACEFREE: pi_racefree c1)
       (PI_STEPS: rtc (pi_step tid) c1 c2)
       (STEP: small_step e tid c2 c3):
@@ -138,26 +173,37 @@ Proof.
   unfold pi_machine_event. destruct (ThreadEvent.is_reading e) as [[[]]|] eqn:E; auto. ii.
   inv H.
   assert (tid <> tid0).
-  { admit. }
+  { admit. (* consistency required *) }
   exploit PI_CONSISTENT.
   { erewrite <- rtc_pi_step_find; eauto. congr. }
   { eapply rtc_implies; try apply PI_STEPS; eauto.
     i. econs; eauto.
   }
-  i. des. exploit PI_RACEFREE.
+  i. des.
+  exploit pi_steps_promise; eauto.
+  { rewrite PROMISES0. apply Memory.bot_get. }
+  i. des.
+  exploit PI_RACEFREE.
   { etrans.
     - eapply rtc_implies; try apply PI_STEPS; eauto.
-      i. econs; eauto.
-    - eapply rtc_implies; try apply STEPS; eauto.
-      i. econs; eauto.
+      i. econs. eauto.
+    - eapply rtc_implies; try apply STEPS0; eauto.
+      i. econs. eauto.
   }
-  { admit. (* race_rw *) }
-  { i. des. admit. (* rel -> contradiction *) }
+  { (* TODO: is ThreadEvent.is_reading/writing neccessary? *)
+    inv STEP. inv STEP0; [by inv E|].
+    erewrite <- rtc_pi_step_find in TID0; eauto.
+    inv STEP; inv E.
+    - econs; eauto; ss.
+    - econs; eauto; ss.
+  }
+  i. des. destruct ord2; inv x0; inv x1.
 Admitted.
 
 Lemma pi_consistent_rtc_tau_small_step_pi
       tid c1 c2 c3
       (PI_CONSISTENT: pi_consistent c1)
+      (CONSISTENT: Configuration.consistent c1)
       (PI_RACEFREE: pi_racefree c1)
       (PI_STEPS: rtc (pi_step tid) c1 c2)
       (STEP: rtc (tau_small_step tid) c2 c3):
@@ -196,6 +242,7 @@ Lemma pi_consistent_pi_step_pi_consistent
 Proof.
   ii. destruct (Ident.eq_dec tid tid0).
   - subst tid0.
+    (* TODO: sketch the proof *)
     admit.
   - eapply PI_CONSISTENT.
     + erewrite <- rtc_pi_step_find; eauto.
--- src/lang/Event.v
+++ src/lang/Event.v
@@ -92,4 +92,18 @@ Module ProgramEvent.
   | fence (ordr ordw:Ordering.t)
   | syscall (e:Event.t)
   .
+
+  Definition is_reading (e:t): option (Loc.t * Ordering.t) :=
+    match e with
+    | read loc _ ord => Some (loc, ord)
+    | update loc _ _ ordr _ => Some (loc, ordr)
+    | _ => None
+    end.
+
+  Definition is_writing (e:t): option (Loc.t * Ordering.t) :=
+    match e with
+    | write loc _ ord => Some (loc, ord)
+    | update loc _ _ _ ordw => Some (loc, ordw)
+    | _ => None
+    end.
 End ProgramEvent.
